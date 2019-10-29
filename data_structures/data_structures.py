bf5e4aaf5cbce0ffdfd23fbe933fa4f3be90f5c5
Yanick J. Steinbeck <yanick.steinbeck@gmail.com>
2019-10-29 15:56:34 +0100

    BST: Adds hibbard deletion and some ordered operations

--- data_structures/data_structures.py
+++ data_structures/data_structures.py
@@ -93,9 +93,9 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
         self.upserted_node = None
-    
-    class _Node:
-        def __init__(self, key=None, value=None, left=None, right=None):
+
+    class Node:
+        def __init__(self, key, value=None, left=None, right=None):
             self.key, self.value, self.left, self.right = key, value, left, right
 
     def put(self, key, value):
@@ -103,36 +103,105 @@ class BinarySearchTree:
         return self.upserted_node
 
     def _put(self, node, key, value):
-        if node == None:
-            self.upserted_node = self._Node(key, value)
+        if node is None:
+            self.upserted_node = self.Node(key, value)
             return self.upserted_node
+        if key > node.key:
+            node.right = self._put(node.right, key, value)
         elif key < node.key:
             node.left = self._put(node.left, key, value)
-        elif key > node.key:
-            node.right = self._put(node.right, key, value)
-        elif node.key == key:
+        elif key == node.key:
             node.value = value
             self.upserted_node = node
         return node
 
-    def get(self, key):
-        x = self.root
-        while x is not None:
-            if x.key == key:
-                print(f"Found {key}:{x.value}")
-                return x.value
-            if x.key > key:
-                x = x.left
-            if x.key < key:
-                x = x.right
-        print(f"Could not find {key}")
-        return
+    def del_min(self):
+        if self.root is None:
+            return False
+        self.root = self._del_min(self.root)
+
+    def _del_min(self, node):
+        if node.left is None: 
+            return node.right
+        node.left = self._del_min(node.left)
+        return node
 
     def delete(self, key):
+        if self.root is None:
+            return False
+        self.root = self._delete(self.root, key)
+
+    def _delete(self, node, key):
+        # Hibbard deletion 
+        if node is None:
+            return None
+        if key == node.key:
+            if node.left is None and node.right is None:
+                return None
+            elif node.left is None and node.right is not None:
+                return node.right
+            elif node.left is not None and node.right is None:
+                return node.left
+            elif node.left is not None and node.right is not None:
+                min_node = self.min(node.right)
+                new_key, new_value = min_node.key, min_node.value
+                node.right = self._del_min(node.right)
+                node.key, node.value = new_key, new_value
+        elif key > node.key:
+            node.right = self._delete(node.right, key)
+        elif key < node.key:
+            node.left = self._delete(node.left, key)
+        return node
+
+    def find(self, key):
+        node = self.root
+        while node is not None:
+            if key == node.key:
+                return node
+            elif key > node.key:
+                node = node.right
+            elif key < node.key:
+                node = node.left
+        return False
+
+    def get(self, key):
+        node = self.find(key)
+        return node.value if node else False
+
+    def max(self, node=None):
+        node = node or self.root
+        while True:
+            if node.right is None:
+                return node
+            else: 
+                node = node.right
+
+    def min(self, node=None):
+        node = node or self.root
+        while True:
+            if node.left is None:
+                return node
+            else: 
+                node = node.left
+
+    def is_empty(self, key):
+        return self.root is not None
+
+    def size(self, key):
         pass
 
-    def iterator(self):
-        pass
+    def keys(self):
+        q = Queue()
+        self.traverse_inorder(self.root, q)
+        return q
+
+    def traverse_inorder(self, node, queue):
+        if node is None: 
+            return
+        self.traverse_inorder(node.left, queue)
+        queue.enqueue(node.key)
+        self.traverse_inorder(node.right, queue)
+
 
 class Stack_Array:
     def __init__(self):
@@ -149,21 +218,20 @@ class Stack_Array:
 
     def size(self):
         return len(self.a)
-    
-
-class Node:
-    def __init__(self, node_val=None, next_node=None):
-        self.node_val = node_val
-        self.next_node = None
 
 
 class Stack_Singly_Linked_List:
     def __init__(self, init_val):
-        self.head = Node(init_val, next_node=None)
+        self.head = self.Node(init_val, next_node=None)
         self.print_list()
 
+    class Node:
+        def __init__(self, node_val=None, next_node=None):
+            self.node_val = node_val
+            self.next_node = None
+
     def push(self, new_val):
-        new_node = Node(node_val=new_val)
+        new_node = self.Node(node_val=new_val)
         new_node.next_node = self.head
         self.head = new_node
 
@@ -178,6 +246,17 @@ class Stack_Singly_Linked_List:
                     break
             print(next_node.node_val)
             next_node = next_node.next_node
+
+class Queue:
+    def __init__(self):
+        self.q = []
+
+    def enqueue(self, key):
+        self.q.append(key)
+        return len(self.q)-1
+
+    def dequeue(self):
+        return self.q.pop(0)
 
 
 def client():
@@ -198,8 +277,10 @@ def client():
 
 if __name__ == '__main__':
     BST = BinarySearchTree()
-    print(BST.put(2,2))
+    print(BST.put(1,1))
     print(BST.put(5,5))
-    print(BST.put(4,4))
-    print(BST.put(4,3))
-
+    print(BST.put(3,3))
+    print(BST.put(0.5,0.5))
+    print(BST.put(2,2))
+    BST.delete(1)
+    BST.delete(5)
