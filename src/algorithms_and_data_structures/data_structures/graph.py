0e3d5465b6e76a17e754a3a1dc3760e7f3b1a7a6
Yanick J. Steinbeck <yanick.steinbeck@gmail.com>
2020-01-03 19:28:34 +0100

    adds Digraph class and refactors Graph class

--- src/algorithms_and_data_structures/data_structures/graph.py
+++ src/algorithms_and_data_structures/data_structures/graph.py
@@ -38,34 +38,64 @@ class ListItem:
       item = item.next
     return item
 
+class Digraph:
+  # TODO Inheriting from Graph? Or rather: Refactor graph based on Digraph
+  def __init__(self, vertex_count):
+    """Implements a digraph that supports self loops but not parallel edges"""
+    self.vertex_count = vertex_count
+    self.adj_list = [set() for v in range(vertex_count)]
+
+  def __str__(self):
+    return (f'Adj List: {[list(v) for v in self.adj_list]}\n'
+            f'**********************************************')
+
+  def add_edge(self, v, w):
+    """adds a directional edge from v to w"""
+    self.adj_list[v].add(w)
+
+  def adj(self, v):
+    """returns a list of vertices adjacent to v"""
+    return list(self.adj_list[v])
+
+  def e(self):
+    """returns number of edges"""
+    return len([w for v in self.adj_list for w in v])
+
+  def edge_between(self, v, w):
+    """returns True if there is a directional edge from v to w, else False"""
+    return w in self.adj_list[v]
+
+  def v(self):
+    """returns number of vertices"""
+    return self.vertex_count
+
+  def topological_sort(self):
+    """implements https://en.wikipedia.org/wiki/Topological_sorting#Depth-first_search"""
+    # TODO
+    pass
 
 class Graph:
   # TODO: Missing error handling
-  # TODO: What about self loops?
-  # TODO: What about parallel edges?
   def __init__(self, vertex_count):
-    # implementing three graph representation variants for illustration purposes:
-    # list of edges, adjaceny matrix, adjacency list
+    """implementing three graph representation variants for illustration purposes:
+       list of edges, adjaceny matrix, adjacency list.
+       Supports self loops but not parallel edges."""
     self.vertex_count = vertex_count
     self.edge_list = []
     self.adj_matrix = ([
-      [0 for i in range(vertex_count)] for i in range(vertex_count)
+      [0 for v in range(vertex_count)] for w in range(vertex_count)
     ])
-    self.adj_list = [ListItem(None) for i in range(vertex_count)]
-    # TODO: Switch adjacency list to array of arrays for simplicity and cache perf
-    # TODO: Bonus: Switch adjacency list to list of dicts for ultimate perf
+    self.adj_list = [set() for v in range(vertex_count)]
 
   def __str__(self):
     return (f'Edge List: {self.edge_list}\n'
             f'Adj Matrix: \n{DataFrame(self.adj_matrix)}\n' 
-            f'Adj List: {[v.all() for v in self.adj_list]}\n'
+            f'Adj List: {[v for v in self.adj_list]}\n'
             f'**********************************************')
 
   def add_edge(self, v, w):
     """adds edge between v and w"""
-    if v == w: 
-      return
-  
+ 
     # add edge to edge list
     (self.edge_list.append((v, w)) 
       if not (v, w) in self.edge_list 
@@ -76,50 +106,54 @@ class Graph:
     self.adj_matrix[v][w] = self.adj_matrix[w][v] = 1
     
     # add edge to adj list
-    if not self.adj_list[v].contains(w):
-      self.adj_list[v].final().next = ListItem(w)
-      self.adj_list[w].final().next = ListItem(v)
+    if not w in self.adj_list[v]:
+      self.adj_list[v].add(w)
+      self.adj_list[w].add(v)
+
+  def adj(self, v):
+    """returns iterable of vertices adjacent to v from adj list"""
+    return self._adj_from_adj_list(v)
+
+  def e(self):
+    """returns number of edges"""
+    return self._e_from_adj_list()
 
   def edge_between(self, v, w):
     """returns True if there is and edge between v and w, else False"""
     # using only adjacency list here to not overcomplicate this code
-    return self.adj_list[v].contains(w)
-
-  def adj(self, v):
-    """returns iterable of vertices adjacent to v from adj list"""
-    return self.adj_from_adj_list(v)
-
-  def adj_from_edge_list(self, v):
-    """returns iterable of vertices adjacent to v from edge list"""
-    return [edge[0] if edge[1] == v else edge[1]
-            for edge in self.edge_list
-            if edge[0] == v or edge[1] == v]
-
-  def adj_from_adj_matrix(self, v):
-    """returns iterable of vertices adjacent to v from adj matrix"""
-    return [index for (index, value) in enumerate(self.adj_matrix[v]) if value == 1]
-
-  def adj_from_adj_list(self, v):
-    """returns iterable of vertices adjacent to v from adj list"""
-    return self.adj_list[v].all()
+    return w in self.adj_list[v]
 
   def v(self):
     """returns number of vertices"""
     return self.vertex_count
 
-  def e_from_edge_list(self):
+  def _adj_from_edge_list(self, v):
+    """returns iterable of vertices adjacent to v from edge list"""
+    return [edge[0] if edge[1] == v else edge[1]
+            for edge in self.edge_list
+            if edge[0] == v or edge[1] == v]
+
+  def _adj_from_adj_matrix(self, v):
+    """returns iterable of vertices adjacent to v from adj matrix"""
+    return [index for (index, value) in enumerate(self.adj_matrix[v]) if value == 1]
+
+  def _adj_from_adj_list(self, v):
+    """returns iterable of vertices adjacent to v from adj list"""
+    return self.adj_list[v]
+
+  def _e_from_edge_list(self):
     """returns number of edges from edge list"""
     return len(self.edge_list)
 
-  def e_from_adj_matrix(self):
+  def _e_from_adj_matrix(self):
     """returns number of edges from adj matrix"""
     # dividing by 2 because each edge is represented twice: [v][w] and [w][v]
     return reduce(lambda a, b: a + b.count(1), self.adj_matrix, 0) // 2
 
-  def e_from_adj_list(self):
+  def _e_from_adj_list(self):
     """returns number of edges from adj list"""
     # dividing by 2 because each edge is represented twice
-    return sum([vertex.count() for vertex in self.adj_list]) // 2
+    return sum([len(vertex_set) for vertex_set in self.adj_list]) // 2
 
 
 class GraphConnectivityQueries:
@@ -132,18 +166,6 @@ class GraphConnectivityQueries:
     self.vertex_visited = [False for v in range(self.vertex_count)]
     self.component_count = 0
     self._preprocess()
-
-  def _preprocess(self):
-    # would run faster if we add a (duplicate) tweaked dfs/bfs method to this class
-    # this would mean duplicate dfs/bfs code but we could get rid of the nested for loop
-    for v in range(self.vertex_count):
-      if self.vertex_visited[v] == False:
-        self.component_count += 1
-        GS = GraphSearch(self.graph, v)
-        for vertex, visited in enumerate(GS.vertex_visited):
-          if visited:
-            self.vertex_visited[vertex] = True
-            self.component_of[vertex] = self.component_count
 
   def connected(self, v, w):
     """returns True if v and w are connected, else False"""
@@ -159,11 +181,23 @@ class GraphConnectivityQueries:
     """returns the connected component id for a given vertex"""
     return self.component_of[v]
 
+  def _preprocess(self):
+    # would run faster if we add a (duplicate) tweaked dfs/bfs method to this class
+    # this would mean duplicate dfs/bfs code but we could get rid of the nested for loop
+    for v in range(self.vertex_count):
+      if self.vertex_visited[v] == False:
+        self.component_count += 1
+        GS = GraphSearch(self.graph, v)
+        for vertex, visited in enumerate(GS.vertex_visited):
+          if visited:
+            self.vertex_visited[vertex] = True
+            self.component_of[vertex] = self.component_count
+
 
 class GraphSearch:
   # TODO split into own module
-  # TODO enable choice between dfs and bfs for user
-  # TODO (maybe) enable finding of multiple paths via generator
+  # TODO enable choice between dfs, dfs iterative and bfs for user
+  # TODO bonus: enable finding of multiple paths via generator
   def __init__(self, graph, source_vertex):
     self.graph = graph
     self.source = source_vertex
@@ -172,6 +206,24 @@ class GraphSearch:
     self.parent = [None for v in range(self.vertex_count)]
     self.cycle = []
     self._bfs()
+
+  def is_acyclic(self):
+    return bool(self.cycle)
+
+  def source_has_path_to(self, v):
+    return self.vertex_visited[v]
+
+  def source_path_to(self, v):
+    if not self.source_has_path_to(v):
+      return []
+    path = [v]
+    parent = self.parent[v]
+    while parent is not self.source: 
+      path.append(parent)
+      parent = self.parent[parent]
+    path.append(self.source)
+    path.reverse()
+    return path
 
   def _bfs(self):
     queue = [self.source]
@@ -221,23 +273,6 @@ class GraphSearch:
           return True
         visited[w] = True
 
-  def is_acyclic(self):
-    return bool(self.cycle)
-
-  def source_has_path_to(self, v):
-    return self.vertex_visited[v]
-
-  def source_path_to(self, v):
-    if not self.source_has_path_to(v):
-      return []
-    path = [v]
-    parent = self.parent[v]
-    while parent is not self.source: 
-      path.append(parent)
-      parent = self.parent[parent]
-    path.append(self.source)
-    path.reverse()
-    return path
 
 # class GraphCycles:
 # DOESNT WORK - SEEMS VERY COMLPEX
@@ -302,6 +337,67 @@ class GraphSearch:
 
 
 # TODO translate this into a proper unit test
+# if __name__ == "__main__":
+#   DG = Digraph(10)
+#   print(DG)
+#   DG.add_edge(0,9)
+#   DG.add_edge(0,9)
+#   print(DG)
+#   DG.add_edge(0,8)
+#   DG.add_edge(0,7)
+#   DG.add_edge(1,2)
+#   DG.add_edge(1,1)
+#   DG.add_edge(2,6)
+#   DG.add_edge(2,9)
+#   DG.add_edge(9,2)
+#   DG.add_edge(3,5)
+#   DG.add_edge(6,9)
+#   print(f'Vertex count: {DG.v()}')
+#   print(f'Edge count: {DG.e()}')
+#   print(DG)
+#   print('Edges for vertex 0:')
+#   print(DG.adj(0))
+#   print('Edges for vertex 9:')
+#   print(DG.adj(9))
+#   print('Edges for vertex 5:')
+#   print(DG.adj(5))
+#   GS = GraphSearch(DG, 0)
+#   print(GS.source_has_path_to(2))
+#   print('Path from source to 2:', GS.source_path_to(2))
+#   print(GS.source_has_path_to(3))
+#   print('Path from source to 3:', GS.source_path_to(3))
+#   print('Arbitrary cycle (if any):', GS.cycle)
+  # CC = GraphConnectivityQueries(DG)
+  # print(CC)
+  # assert CC.connected(0, 1) == True
+  # assert CC.connected(0, 3) == False
+  # assert CC.connected(3, 4) == False
+  # assert CC.id(0) == 1
+  # assert CC.id(1) == 1
+  # assert CC.id(4) == 3
+  # assert CC.count() == 3
+
+
+# Adj List: [[], [], [], [], [], [], [], [], [], []]
+# **********************************************
+# Adj List: [[9], [], [], [], [], [], [], [], [], []]
+# **********************************************
+# Vertex count: 10
+# Edge count: 10
+# Adj List: [[8, 9, 7], [1, 2], [9, 6], [5], [], [], [9], [], [], [2]]
+# **********************************************
+# Edges for vertex 0:
+# [8, 9, 7]
+# Edges for vertex 9:
+# [2]
+# Edges for vertex 5:
+# []
+# True
+# Path from source to 2: [0, 9, 2]
+# False
+# Path from source to 3: []
+# Arbitrary cycle (if any): [0, 9, 6, 2, 9, 0]
+
 if __name__ == "__main__":
   G = Graph(10)
   print(G)
@@ -317,21 +413,21 @@ if __name__ == "__main__":
   G.add_edge(9,2)
   G.add_edge(3,5)
   G.add_edge(6,9)
-  print(f'Vertex count: {G.vertex_count}')
-  print(f'Edge count: {G.e_from_edge_list()} / {G.e_from_adj_matrix()} / {G.e_from_adj_list()}')
+  print(f'Vertex count: {G.v()}')
+  print(f'Edge count: {G._e_from_edge_list()} / {G._e_from_adj_matrix()} / {G._e_from_adj_list()}')
   print(G)
   print('Edges for vertex 0:')
-  print(G.adj_from_edge_list(0))
-  print(G.adj_from_adj_matrix(0))
-  print(G.adj_from_adj_list(0))
+  print(G._adj_from_edge_list(0))
+  print(G._adj_from_adj_matrix(0))
+  print(G._adj_from_adj_list(0))
   print('Edges for vertex 9:')
-  print(G.adj_from_edge_list(9))
-  print(G.adj_from_adj_matrix(9))
-  print(G.adj_from_adj_list(9))
+  print(G._adj_from_edge_list(9))
+  print(G._adj_from_adj_matrix(9))
+  print(G._adj_from_adj_list(9))
   print('Edges for vertex 5:')
-  print(G.adj_from_edge_list(5))
-  print(G.adj_from_adj_matrix(5))
-  print(G.adj_from_adj_list(5))
+  print(G._adj_from_edge_list(5))
+  print(G._adj_from_adj_matrix(5))
+  print(G._adj_from_adj_list(5))
   GS = GraphSearch(G, 0)
   print(GS.source_has_path_to(6))
   print('Path from source to 6:', GS.source_path_to(6))
@@ -348,10 +444,39 @@ if __name__ == "__main__":
   assert CC.id(4) == 3
   assert CC.count() == 3
 
+# Edge List: []
+# Adj Matrix: 
+#    0  1  2  3  4  5  6  7  8  9
+# 0  0  0  0  0  0  0  0  0  0  0
+# 1  0  0  0  0  0  0  0  0  0  0
+# 2  0  0  0  0  0  0  0  0  0  0
+# 3  0  0  0  0  0  0  0  0  0  0
+# 4  0  0  0  0  0  0  0  0  0  0
+# 5  0  0  0  0  0  0  0  0  0  0
+# 6  0  0  0  0  0  0  0  0  0  0
+# 7  0  0  0  0  0  0  0  0  0  0
+# 8  0  0  0  0  0  0  0  0  0  0
+# 9  0  0  0  0  0  0  0  0  0  0
+# Adj List: [set(), set(), set(), set(), set(), set(), set(), set(), set(), set()]
+# **********************************************
+# Edge List: [(0, 9)]
+# Adj Matrix: 
+#    0  1  2  3  4  5  6  7  8  9
+# 0  0  0  0  0  0  0  0  0  0  1
+# 1  0  0  0  0  0  0  0  0  0  0
+# 2  0  0  0  0  0  0  0  0  0  0
+# 3  0  0  0  0  0  0  0  0  0  0
+# 4  0  0  0  0  0  0  0  0  0  0
+# 5  0  0  0  0  0  0  0  0  0  0
+# 6  0  0  0  0  0  0  0  0  0  0
+# 7  0  0  0  0  0  0  0  0  0  0
+# 8  0  0  0  0  0  0  0  0  0  0
+# 9  1  0  0  0  0  0  0  0  0  0
+# Adj List: [{9}, set(), set(), set(), set(), set(), set(), set(), set(), {0}]
 # **********************************************
 # Vertex count: 10
-# Edge count: 7 / 7 / 7
-# Edge List: [(0, 9), (0, 8), (0, 7), (1, 2), (2, 6), (2, 9), (3, 5)]
+# Edge count: 8 / 8 / 8
+# Edge List: [(0, 9), (0, 8), (0, 7), (1, 2), (2, 6), (2, 9), (3, 5), (6, 9)]
 # Adj Matrix: 
 #    0  1  2  3  4  5  6  7  8  9
 # 0  0  0  0  0  0  0  0  1  1  1
@@ -360,27 +485,27 @@ if __name__ == "__main__":
 # 3  0  0  0  0  0  1  0  0  0  0
 # 4  0  0  0  0  0  0  0  0  0  0
 # 5  0  0  0  1  0  0  0  0  0  0
-# 6  0  0  1  0  0  0  0  0  0  0
+# 6  0  0  1  0  0  0  0  0  0  1
 # 7  1  0  0  0  0  0  0  0  0  0
 # 8  1  0  0  0  0  0  0  0  0  0
-# 9  1  0  1  0  0  0  0  0  0  0
-# Adj List: [[9, 8, 7], [2], [1, 6, 9], [5], [], [3], [2], [0], [0], [0, 2]]
+# 9  1  0  1  0  0  0  1  0  0  0
+# Adj List: [{8, 9, 7}, {2}, {1, 6, 9}, {5}, set(), {3}, {9, 2}, {0}, {0}, {0, 2, 6}]
 # **********************************************
 # Edges for vertex 0:
 # [9, 8, 7]
 # [7, 8, 9]
-# [9, 8, 7]
+# {8, 9, 7}
 # Edges for vertex 9:
-# [0, 2]
-# [0, 2]
-# [0, 2]
+# [0, 2, 6]
+# [0, 2, 6]
+# {0, 2, 6}
 # Edges for vertex 5:
 # [3]
 # [3]
-# [3]
+# {3}
 # True
-# Path from source to 6: [0, 9, 6]
+# Path from source to 6: [0, 9, 2, 6]
 # False
 # Path from source to 3: []
 # Arbitrary cycle (if any): [0, 9, 6, 2, 9, 0]
-# <__main__.GraphConnectivityQueries object at 0x7f246aa7a5d0>
+# <__main__.GraphConnectivityQueries object at 0x7f87af109110>
