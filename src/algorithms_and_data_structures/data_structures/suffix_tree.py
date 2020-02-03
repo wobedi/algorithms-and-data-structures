b802e570036e4f206da305fb4bdeddbb597b6a78
Yanick J. Steinbeck <yanick.steinbeck@gmail.com>
2020-02-03 14:58:51 +0100

    comment out (unimplemented) suffix tree

--- src/algorithms_and_data_structures/data_structures/suffix_tree.py
+++ src/algorithms_and_data_structures/data_structures/suffix_tree.py
@@ -1,15 +1,3 @@
-class SuffixEdge:
-  def __init__(self, v, w, offset: int, size: int):
-    self.v = v
-    self.w = w
-    self.offset = offset  # only for SuffixTree, not for SuffixTrie
-    self.size = size  # only for SuffixTree, not for SuffixTrie
-
-
-class SuffixNode:
-  def __init__(self):
-    self.children: dict(SuffixNode) = dict()
-    self.occurrences = []
 
 
 def _input_validation(func):
@@ -61,9 +49,9 @@ class SuffixTrie:
     node = self._find_last_node_of(pattern)
     if not node:
       return 0
-    return self._dfs(node, [0])  # TODO: hackish to use list here to pass by reference
+    return self._dfs(node, [0])
 
-  def _dfs(self, node: SuffixNode, count):
+  def _dfs(self, node: {}, count):
     if self.terminal_char in node:
       count[0] += 1
     for (_, child) in node.items():
@@ -79,7 +67,7 @@ class SuffixTrie:
     return self.terminal_char in node
 
   @_input_validation
-  def _find_last_node_of(self, pattern: str) -> SuffixNode:
+  def _find_last_node_of(self, pattern: str) -> {}:
     node = self.root
     for c in pattern:
       if c not in node: 
@@ -88,35 +76,71 @@ class SuffixTrie:
     return node
 
   
-class SuffixTree:
-  """ Implements a suffix trie that enables O(L) substring search where L is the length of the substring
-      Currently only supports one single contiguous string, not a collection of strings (non-generalized)."""
-  def __init__(self, string: str):
-    """ Simple construction in O(N²). Ukkonen's algorithm would enable construction in O(N)
-        A suffix tree uses O(N) space (whereas a suffix trie uses O(N²))"""
-        # terminal char
-        # maybe: construction variant based on trie vs not
-    pass
+if __name__ == '__main__':
+  STrie = SuffixTrie('abcdefghijklmnopqrstuvwxyznopqnopqfghijklmnobcdefepoqijrmnopqrsoiweroiwn')
+  print(STrie)
+  pattern_list = ['opq', 'abc', 'oiwn', 'mnopqrsoiweroiwn', 'fg']
+  for pattern in pattern_list:
+    print(f'Pattern {pattern} appears {STrie.count(pattern)} times in T')
+    print(f'Pattern {pattern} is a suffix of T: {STrie.is_suffix_of_T(pattern)}')
+  # STrie.count('werewrew!$er')
+  # STrie = SuffixTrie('werewrew!$er')
 
-  def count(self, pattern: str) -> int:
-    """Returns the number of times a pattern p appears in text self.T"""
-    pass
-
-  def occurrences(self, pattern: str) -> [int]:
-    """Returns all occurrences of pattern p in self.T by their offset"""
-    pass
-
-  def is_suffix_of_T(self, pattern: str) -> bool:
-    """Returns True if pattern is a suffix of self.T, else False"""
-    pass
+# class SuffixEdge:
+#   def __init__(self, offset: int, length: int):
+#     self.offset = offset
+#     self.length = length
+#     self.next = {}
 
 
-  if __name__ == '__main__':
-    STrie = SuffixTrie('abcdefghijklmnopqrstuvwxyznopqnopqfghijklmnobcdefepoqijrmnopqrsoiweroiwn')
-    print(STrie)
-    pattern_list = ['opq', 'abc', 'oiwn', 'mnopqrsoiweroiwn', 'fg']
-    for pattern in pattern_list:
-      print(f'Pattern {pattern} appears {STrie.count(pattern)} times in T')
-      print(f'Pattern {pattern} is a suffix of T: {STrie.is_suffix_of_T(pattern)}')
-    # STrie.count('werewrew!$er')
-    # STrie = SuffixTrie('werewrew!$er')
+# class SuffixTree:
+#   """ Implements a suffix trie that enables O(L) substring search where L is the length of the substring
+#       Currently only supports one single contiguous string, not a collection of strings (non-generalized)."""
+#   def __init__(self, string: str, terminal_char='$', suffix_trie=None):
+#     """ Simple construction in O(N²). Ukkonen's algorithm would enable construction in O(N)
+#         A suffix tree uses O(N) space (whereas a suffix trie uses O(N²))"""
+#     self.root: dict(SuffixEdge) = {}  # keys = first chars
+#     self.terminal_char = terminal_char
+#     self.string = string
+#     if suffix_trie:
+#       self.construct_via_suffix_trie(suffix_trie)
+#     else:
+#       self.construct_from_scratch()
+
+#   def construct_via_suffix_trie(self, suffix_trie: SuffixTrie):
+#     pass
+#     # node = suffix_trie.root
+#     # dfs
+#     # stack chars
+#     # as you pop, store in reverse until you hit branching one, append it there
+#     # ...
+
+#   def construct_from_scratch(self):
+#     for offset in range(0, len(self.string)):
+#       node = self.root
+#       for index, char in enumerate(self.string[offset:]):
+#         edge = node.get(char, None)
+#         # case 0: add when its not there
+#         if not edge:
+#           node[char] = SuffixEdge(offset + index)
+        
+#         # case 1: stretch when it's there but not single
+#         if edge.length > 1:
+#           for i in range(edge.length)
+
+        
+#         # case 2: traverse if its there and single
+        
+
+#   def count(self, pattern: str) -> int:
+#     """Returns the number of times a pattern p appears in text self.T"""
+#     pass
+
+#   def occurrences(self, pattern: str) -> [int]:
+#     """Returns all occurrences of pattern p in self.T by their offset"""
+#     pass
+
+#   def is_suffix_of_T(self, pattern: str) -> bool:
+#     """Returns True if pattern is a suffix of self.T, else False"""
+#     pass
+
