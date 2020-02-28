a401e6f8558550586126b475cf424a3393f2d58a
Yanick J. Steinbeck <yanick.steinbeck@gmail.com>
2020-02-28 16:50:01 +0100

    Rm pytest-benchmark from dependencies, add pandas and pytest-print to them

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='algorithms_and_data_structures',
     license='MIT',
-    description='Implements various algorithms and data structures',
+    description='Implements various algorithms and plots their performance.',
     author='Yanick J. Steinbeck',
     author_email='yanick.steinbeck+githubdsa@gmail.com',
     url='https://github.com/wobedi/algorithms-and-data-structures',
