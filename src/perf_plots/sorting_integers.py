dfc35215bc8da3d27993fc9608c44f85806b7dc0
Yanick J. Steinbeck <yanick.steinbeck@gmail.com>
2020-05-19 17:54:36 +0200

    Add integer sorting perf plot

---
+++ src/perf_plots/sorting_integers.py
@@ -0,0 +1,39 @@
+from random import sample
+
+import perfplot
+
+from src.implementations.sorting.basic_sorts import \
+    selection_sort, insertion_sort, shell_sort
+from src.implementations.sorting.heapsort import heapsort
+from src.implementations.sorting.mergesort import \
+mergesort_iterative, mergesort_recursive
+from src.implementations.sorting.quicksort import quicksort
+from src.implementations.sorting.radixsort import radixsort_LSD
+from src.perf_plots.config import SAMPLE_SIZES
+
+SAMPLE_SIZE = SAMPLE_SIZES['SORTING_INTEGERS']
+
+
+if __name__ == '__main__':
+    output = perfplot.bench(
+        setup=lambda n: sample(range(n + 1), n + 1),
+        kernels=[
+            lambda a: selection_sort(a),
+            lambda a: insertion_sort(a),
+            lambda a: shell_sort(a),
+            lambda a: heapsort(a),
+            lambda a: mergesort_recursive(a),
+            lambda a: mergesort_iterative(a),
+            # lambda a: quicksort(a),
+        ],
+        labels=['selection sort', 'insertion sort', 'shell sort', 'heapsort', 
+                'mergesort (rec)', 'mergesort (itr)'],
+        xlabel="Array of N integers in [0, N)",
+        title='Sorting an array of integers',
+        n_range=[n for n in range(1, SAMPLE_SIZE)],
+        equality_check=None
+    )
+
+    output.save(f'output/sorting-integers-plot_sample-size-{SAMPLE_SIZE}.png',
+                transparent=False,
+                bbox_inches="tight")
