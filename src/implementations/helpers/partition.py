f8234f5f469c3c41579f69853e33055dcd174cec
Yanick J. Steinbeck <yanick.steinbeck@gmail.com>
2020-06-08 14:31:38 +0200

    Unit test for partition helper*

--- src/implementations/helpers/partition.py
+++ src/implementations/helpers/partition.py
@@ -16,3 +16,11 @@ def three_way_partition(arr: list, lower: int, upper: int) -> (int, int):
         else:
             i += 1
     return lt, gt
+
+
+if __name__ == '__main__':
+    arr = [4, 5, 4, 4, 1, 8, 3, 2, 9, 6]  # [1, 2, 3, 4, 4, 4, 5, 6, 7, 8, 9]
+    lt, gt = three_way_partition(arr, 0, len(arr) - 1)
+    print(f'lt: {lt}, gt: {gt}')
+    assert lt == 3
+    assert gt == 5
